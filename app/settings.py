8c7961cc6c7336c41b3337a86dc34d02080f926b
KarlP17123 <karl.park@telia.com>
2023-03-09 15:20:53 +0000

    new adds

--- app/settings.py
+++ app/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['minetest.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['mine.herokuapp.com', 'localhost']
 
 
 # Application definition
